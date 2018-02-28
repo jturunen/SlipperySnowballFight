cac4dc82e0ebc2b1dfd869f54f188a4701fcb5f5
Teemu Karjalainen <t5kate01@students.oamk.fi>
2018-02-28 19:26:04 +0200

    Basic firing mechanic

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject projectilePrefab;
+    public Transform projectileSpawn;
+
+    public float projectileVelocity;
+
+    AudioSource audio;
+    public AudioClip audioClipProjectile;
+
+
+    // Use this for initialization
+    void Start () {
+        audio = gameObject.AddComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        audio.PlayOneShot(audioClipProjectile);
+
+        // Create the Bullet from the Bullet Prefab
+        var projectile = (GameObject)Instantiate(
+            projectilePrefab,
+            projectileSpawn.position,
+            projectileSpawn.rotation);
+
+        // Add velocity to the bullet
+        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectileVelocity;
+
+        // Destroy the bullet after 10 seconds
+        Destroy(projectile, 10.0f);
+    }
+}
---
+++ Assets/Script/ProjectileController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+
+    public float projectileHitForce;
+    public float projectileDamage;
+
+    private Rigidbody rigidbody;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //all projectile colliding game objects should be tagged "Player" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("HIT!");
+            rigidbody = collider.gameObject.GetComponent<Rigidbody>();
+            rigidbody.AddForce(transform.forward * projectileHitForce);
+            //destroy the projectile
+            Destroy(gameObject);
+        }
+    }
+}
